1ae4f930fae8460a5fce02a70f37802659adb281
Yumin Park <qkrdbals1630@naver.com>
2021-10-14 20:07:30 +0900

    BWOA
    BWOA uplocad

--- Autonomous-Domain-Specific_Model-Generator/readme.md
+++ Autonomous-Domain-Specific_Model-Generator/readme.md
@@ -1,1 +1,23 @@
+# Autonomous Domain-specific Model Generator
 
+## Introduction
+<div style="text-align: justify">
+The main challenging issue to utilize deep learning for various tasks such as content's popularity prediction is to efficiently choose the best-suited deep learning architecture among the various types of deep learning. Currently, domain-specific deep learning models are constructed manually by human experts, where the model construction process is a time-consuming and error-prone process. This is because the human experts need to find out the appropriate neural architectures, training procedures, regularization methods, and hyperparameters of all of these components in order to make their networks do what they are supposed to do with sufficient performance. This process has to be repeated for every application. Because of this, we are developing the opensource platform to autonomously search the appropriate deep learning architecture for problem-specific tasks (i.e., find the appropriate model among feedforward neural networks, recurrent neural networks, etc. )as well as autonomously do the hyperparameters optimization (i.e., find the number of appropriate hidden layers, number of neurons, etc. ).
+</div>
+
+## The progress of Autonomous Domain-specific Model Generator 
+<div style="text-align: justify">
+We implemented the “Autonomous Domain-specific Model Generator” (ADSMG) by using Tensorflow with Keras. Currently, ADSMG is underdevelopement and we implemented the following basic components for the ADSMG as shown in Figure 1. The implemented components are as follows: 1) Data Store, 2) Preprocessing Module, 3) Model Architecture Dictionary, Model Creation Module, Temporary Model Store, Model Training Module, and Model store. Currently, the ADSMG can assess via Jupyter Notebook. In the current version, the Data Store is to store the open dataset from the various sources such as “MovieLens” dataset. The preprocessing module cleans the raw data to get the trainable data for the best-suited model searching and selection process. The Model Architecture Dictionary keeps the general deep learning framework (i.e., Convolutional Neural Network) and rules to construct deep learning models. Then, the Model Creation Module generates the potential deep learning model by using various types of model searching and construction algorithms (i.e., random search). Then, the Model Creation Module store all of the constructed models and the performance of the models in the Tmp Model Store. Next, the Model Training Module chooses the best-suited model and train that model with the large dataset. Finally, the best-suited model is stored at the Model Store for future use.
+</div>
+
+![system_model](https://github.com/kyithar/Autonomous-Domain-specific-Model-Generator-/blob/master/md_figs/system_model.jpg)
+
+Fig. 1 Basic components of Autonomous Domain-specific Model Generator
+
+## Requirements
+
+Tensorflow
+
+Keras
+
+Pandas
---
+++ Resouce allocation module/BWOA.py
@@ -0,0 +1,84 @@
+def WOA(SearchAgents_no, Max_iter, fobj, dim, cases):
+    '''
+    This code is for Discrete Whale Optimization Algorithm(DWOA)
+    "SearchAgents_no" is number of searching agents
+    "Max_iter" is limit of iteration
+    "fobj" denotes the objective function
+    "dim" is number of split num
+    "cases" is number of MEC num
+    '''
+    Leader_pos = np.zeros((1,dim))
+    Leader_score = float('-inf')
+    Leader_score_pre = Leader_score
+    
+    delta = 1e-6
+    todoTol = 0
+    Flag = 0
+
+    Positions = np.zeros((SearchAgents_no, dim))
+    Positions[0] = copy.deepcopy(s_solution)
+    for i in range(1, len(Positions)):
+        for j in range(len(Positions[i])):
+            Positions[i][j] = int(rand.random() * (cases + 1))
+    Convergence_curve = np.zeros((1, Max_iter))
+
+    iter = 0
+
+    while iter < Max_iter and Flag <= 3:
+        # Leader_score = 0
+        for i in range(len(Positions)):
+            for j in range(len(Positions[i])):
+                if Positions[i][j] >= cases:
+                    Positions[i][j] = cases - 1
+                elif Positions[i][j] < 0:
+                    Positions[i][j] = 0
+                Positions[i][j] = math.floor(Positions[i][j])
+            fitness = fobj(Positions[i])
+            if fitness > Leader_score:
+                Leader_score = fitness
+                Leader_pos = copy.deepcopy(Positions[i])
+                print(fitness)
+
+        a = 2 - iter * ((2)/Max_iter)
+        a2 = -1 + iter * ((-1)/Max_iter)
+
+        for i in range(len(Positions)):
+            r1 = rand.random()
+            r2 = rand.random()
+
+            A = 2 * a * r1 - a
+            C = 2 * r2
+
+            p = rand.random()
+
+            b = 1
+            l = (a2-1)*rand.random() + 1
+
+            for j in range(len(Positions[i])):
+                if p < 0.5:
+                    if abs(A) >= 1:
+                        rand_leader_index = math.floor(SearchAgents_no * rand.random())
+                        X_rand = Positions[rand_leader_index]
+                        D_X_rand = abs(C*X_rand[j] - Positions[i][j])
+                        Positions[i][j] = X_rand[j] - A*D_X_rand
+                    elif abs(A) < 1:
+                        D_Leader = abs(C*Leader_pos[j] - Positions[i][j])
+                        Positions[i][j] = Leader_pos[j] - A*D_Leader
+                elif p >= 0.5:
+                    distance2Leader = abs(Leader_pos[j]-Positions[i][j])
+                    Positions[i][j] = distance2Leader*math.exp(b*l)*math.cos(2*math.pi*l) + Leader_pos[j]
+
+        iter = iter + 1
+        Convergence_curve[0][iter-1] = Leader_score
+        if todoTol == 1 and abs(Leader_score - Leader_score_pre) < delta:
+            Flag = Flag + 1
+            # Convergence_curve = Convergence_curve[0][0:iter]
+        
+        Leader_score_pre = Leader_score
+    
+    converg.append(Convergence_curve[0])
+    return Leader_pos
+
+# Fitness for one agent
+def fobj(Position):
+    return fit
